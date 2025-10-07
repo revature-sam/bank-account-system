31c63ab2efa3de5597d4eb8edfa17431d15f4580
sam-gupta-git <sam78748@gmail.com>
2025-10-07 17:26:05 -0500

    Main method comments and closing context

--- src/main/java/com/revature/spring/bankaccount/App.java
+++ src/main/java/com/revature/spring/bankaccount/App.java
@@ -1,6 +1,7 @@
 package com.revature.spring.bankaccount;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -11,12 +12,18 @@ public class App
 {
     public static void main( String[] args )
     {
+        // Initialize Spring Application Context
         ApplicationContext context = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        // Retrieve the AccountService bean
         AccountService accountService = context.getBean("AccountService", AccountService.class);
 
+        // Perform some operations
         accountService.printAccountBalance();
         accountService.performAccountTransaction(1500);
         accountService.applyInterest(5.0, 2);
         accountService.printAccountBalance();
+
+        // Close the context
+        ((ConfigurableApplicationContext) context).close(); 
     }
 }
